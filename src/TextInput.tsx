9091203e79d1ddcc66d65f5ebcd4e082551f3fc7
oligeon-dev <oligeon.dev@gmail.com>
2024-11-04 18:30:00 +0900

    wait focus

--- src/TextInput.tsx
+++ src/TextInput.tsx
@@ -5,10 +5,14 @@ export function TextInput() {
   const inputRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
-    // 初回レンダー時にinput要素にフォーカスを当てる
-    if (inputRef.current) {
-      inputRef.current.focus();
-    }
+    const timer = setTimeout(() => {
+      if (inputRef.current) {
+        inputRef.current.focus();
+      }
+    }, 3000);
+
+    // クリーンアップ関数を返して、コンポーネントがアンマウントされるときにタイマーをクリア
+    return () => clearTimeout(timer);
   }, []);
   return <input type="text" ref={inputRef} className="input" />;
 }
